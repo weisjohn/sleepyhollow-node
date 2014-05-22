ef5b08686098a5380e8cb17ba4e3678f3efb3d14
John Weis <weis.john@gmail.com>
2014-05-22 11:26:11 -0400

    tests wired up to use the sister mmodule, referenced in package.json

--- test.js
+++ test.js
@@ -1,27 +1,32 @@
 
-var sleepyhollow = require('./');
+var assert = require('assert')
+  , sleepyhollow = require('./')
+  , drjekyll = sleepyhollow(['node_modules/sleepyhollow-phantom/index.js'])
+  ;
 
-var drjekyll = sleepyhollow(['phantom.js']);
+// the events we've seen, need to see
+var events = [], needed = ["open", "exit", "ack" ];
 
-drjekyll.on('open', function() {
-    console.log('open')
-})
+// register event listeners
+needed.forEach(function(need) {
+    drjekyll.on(need, function() {
+        events.push(need);
+    });
+});
 
-drjekyll.on('render', function() {
-    console.log('render')
-})
-
-drjekyll.on('exit', function() {
-    console.log('exit')
-    process.exit();
-})
-
+// emit a hello which should trigger an ack
 setInterval(function() {
     drjekyll.emit('hello');
-}, 1e3)
+}, 1e3);
 
-
-
-drjekyll.on('ack', function(value) {
-    console.log('phantomjs:', value)
-})
+// run tests when phantomjs finishes
+drjekyll.on('exit', function() {
+    needed.forEach(function(need) {
+        var found = false;
+        events.forEach(function(event) {
+            if (need == event) found = true;
+        });
+        assert(found, need + " event was not fired");
+    });
+    process.exit();
+});
