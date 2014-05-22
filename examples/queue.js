168b875f5c89cf388b4e177dd439b0ea9d15d38b
John Weis <weis.john@gmail.com>
2014-05-22 16:28:10 -0400

    adding a simple queue example

--- examples/queue.js
+++ examples/queue.js
@@ -1,2 +1,14 @@
 
-var sleepyhollow = require('./index');
+var sleepyhollow = require('../index');
+
+var drjekyll = sleepyhollow('./node_modules/sleepyhollow-phantom/examples/queue.js');
+
+var sites = [ 'http://github.com/', 'http://npmjs.org/', 'http://nodejs.org/' ];
+sites.forEach(function(site) { drjekyll.emit('render', site); });
+
+var renders = 0;
+drjekyll.on('rendered', function() {
+    renders++;
+    if (renders == 3) drjekyll.emit('end');
+})
+drjekyll.on('exit', function() { process.exit(); })
