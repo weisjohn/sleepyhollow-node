f177702dd5d2dd4fed16e11ed3756b99c209fbc5
John Weis <weis.john@gmail.com>
2014-07-26 02:21:06 -0400

    implementing the readBuffer to handle multipart messages

--- index.js
+++ index.js
@@ -35,11 +35,44 @@ function sleepyhollow() {
     }
 
     // the read mechanism that selectively writes
+    var readBuffer = [];
     function read(data) {
+
+        // parse the message to ensure phantom sent correctly
         if (!data) return;
         try { data = JSON.parse(data) }
         catch (e) { console.log(e, data); throw new Error(e); }
-        _emit.apply(sleepyhollow, [data.event, data.message]);
+
+        // if the message has already been converted into a full object,
+        // re-stringify it to be parsed later
+        if (typeof data.message == "object") {
+            data.message = JSON.stringify(data.message);
+        }
+
+        // if there's a message, push onto the readBuffer
+        if (data.message) readBuffer.push(data.message);
+
+        // emit if this data is not multipart or it's the last message
+        if (!data.isMultipart || data.isEof) {
+
+            // build up the args to emit
+            var args = [data.event];
+
+            // join up the buffer, silently fail if not an object
+            var message = readBuffer.join('');
+            if (message && typeof message !== "object") {
+                try { message = JSON.parse(message); }
+                catch (e) { console.log(e, message); throw new Error(e); }
+                args.push(message);
+            }
+
+            // emit the event with variable arguments
+            _emit.apply(sleepyhollow, args);
+
+            // reset the buffer
+            readBuffer = [];
+        }
+
         if (data.event == "ack") return;
         write({ "event": "ack" });
         _write();
