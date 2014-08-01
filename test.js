86d9c7187e83ffa13e476fa66a5d18855b5b6314
Garris <garris.shipon@gmail.com>
2014-08-01 16:19:57 -0700

    updated tests to cover phantom ouput passing
    Now tests for "output" event -- also tests output string against the same source file string brought in via fs.

--- test.js
+++ test.js
@@ -2,10 +2,23 @@
 var assert = require('assert')
   , sleepyhollow = require('./')
   , drjekyll = sleepyhollow('node_modules/sleepyhollow-phantom/test.js')
+  , fs = require('fs')
   ;
+
+var outputStr = null, //Our test data.  Will be returned on our "output" event.
+	inputStr = fs.readFileSync('./node_modules/sleepyhollow-node/testData_10k.html','utf8');//our control data
 
 // the events we've seen, need to see
 var events = [], needed = ["open", "render", "exit", "ack" ];
+
+//additional event case
+needed.push("output");//add to list of required events
+drjekyll.on("output",function(str){
+	// 	console.log(str); //uncomment for debugging
+	outputStr = str;
+	events.push("output");//add to list of received events
+});
+
 
 // register event listeners
 needed.forEach(function(need) {
@@ -14,8 +27,10 @@ needed.forEach(function(need) {
     });
 });
 
+
 // tell mrhyde to open github.com
-setTimeout(function() { drjekyll.emit('page', "https://github.com/"); }, 1e3);
+// setTimeout(function() { drjekyll.emit('page', "https://github.com/"); }, 1e3);
+setTimeout(function() { drjekyll.emit('page', "./node_modules/sleepyhollow-node/testData_10k.html"); }, 1e3);
 drjekyll.on('render', function() { drjekyll.emit("end"); });
 
 // run tests when phantomjs finishes
@@ -26,8 +41,14 @@ drjekyll.on('exit', function() {
         events.forEach(function(event) {
             if (need == event) found = true;
         });
+        console.log('testing event: '+need);
         assert(found, need + " event was not fired");
         if (!found) success = false;
     });
+    console.log('comparing input and output');
+    assert.equal(outputStr, inputStr, "Input and output strings are different. Should be identical.");
+    console.log('TESTING COMPLETE.')
     process.exit(success ? 0 : 1);
 });
+
+
